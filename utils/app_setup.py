b60e893eefbd7aad520bb20745ed991694b89f69
Haosen <haosenli2001@gmail.com>
2022-07-23 01:17:10 -0700

    Updated app_setup.py

--- utils/app_setup.py
+++ utils/app_setup.py
@@ -7,14 +7,15 @@ Run this script to generate a new setup file for the socket application.
 - Edit the 'local' value to be False.
 - Rest are optional.
 
-*To run this locally on a private IP Address (DEFUALT):
+*To run this locally on a private IP address (DEFUALT):
 - Set 'server' to be an empty str.
 - Set 'local' to be True.
+- Set 'local-ip' to the local server's IP address.
 - Rest are optional.
 
 Example usage:
     > python3 app_setup.py
-    > Succesfully updated app setup info.
+    > [COMPLETE] Succesfully updated app setup info.
 """
 import pickle
 
@@ -26,6 +27,7 @@ filepath = 'app_setup.pkl'
 protocol = {
     'server': '', # server ip address
     'local': True,
+    'local_ip': '', # local server ip (i.e. 192.168.0...)
     'port': 5678, # port number
     'header': 64, # header
     'format': 'utf-8', # encoding
@@ -38,4 +40,4 @@ protocol = {
 with open(filepath, 'wb') as f:
     pickle.dump(protocol, f, pickle.HIGHEST_PROTOCOL)
 
-print('Succesfully updated app setup info.')
+print('[COMPLETE] Succesfully updated app setup info.')
