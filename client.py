67b3f5000eb8fb2bfd5a87e95d3763a2f06b58d6
Haosen <haosenli2001@gmail.com>
2022-07-23 01:15:53 -0700

    Updated client.py to detect local or public server.

--- client.py
+++ client.py
@@ -1,21 +1,34 @@
 import socket
 import pickle
-
+import sys
 
 import data_transfer as dt
 
 # load app settings
 SETTINGS = dt.load_settings()
 KEYWORDS = SETTINGS['keywords']
+print(SETTINGS)
 
 # create client
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(SETTINGS['addr'])
+
+# load in local server settings
+if SETTINGS['local']:
+    try:
+        client.connect((SETTINGS['local_ip'], SETTINGS['port']))
+    except:
+        ABORT = True
+        # error message
+        print(f'[ERROR] Please provide the local-ip address for'
+            ' your local server in app_setup.py.')
+else:
+    client.connect(SETTINGS['addr'])
 
 
 def main():
+    global ABORT
     connected = True
-    while connected:
+    while connected and not ABORT: 
         user_input = input('Message: ')
         if user_input == KEYWORDS['disconnect']:
             dt.send_msg(client, KEYWORDS['disconnect'])
